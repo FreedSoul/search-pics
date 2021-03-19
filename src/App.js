d63f789ca3e57924a7930568cd20a78ed1396826
FreedSoul <mattwork07@gmail.com>
2021-03-19 18:58:52 -0500

    added api request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  const [images,setImages] = useState([])
+
+  const onSearchSubmit = async(term) => {
+    const response = await axios.get('search/photos',{
+    params:{ query: term },  
+    headers:{
+        Authorization: 'Client-ID QrqP3ddOejlGIHjbfppba0zq-L0Ks6ikgAfNJZTm9uw'
+      } 
+    });
+    setImages(images + response.data.results)
+    console.log(response.data.results)
+    // images.map(image => <li key='index' >
+    //   <img src=`${image}` />
+    // </li>)
+  }
+  
   return (
     <>
-    
+      <div className="ui ">
+        <SearchBar onSubmit={onSearchSubmit} imageList/>
+        found: {images.length}
+      </div>  
     </>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import 'semantic-ui-css/semantic.min.css'
+import React, { useState } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import './SearchBar.css'
 
 const SearchBar = (props) =>{
+    //const [startSearch,setstartSearch] = useState('')
+    const [term,setTerm] = useState('')
+    const onSearchSubmit = (e) => {
+        e.preventDefault();
+        console.log(`the term ${term} has been submitted!`);
+        props.onSubmit(term)
+    }
+
     return (
-    <div className="ui category search">
-        <div className="ui icon input">
-        <input className="prompt" type="text" placeholder="search something..." />
-            <i className="search icon"></i>
+        <div className="ui segment">
+            <form onSubmit={onSearchSubmit} className="ui form">
+                    {/* <label>buscar</label> */}
+                <div className="field inline">
+                    <input type="text" value={term} onChange={(event)=>{setTerm(event.target.value);console.log(term)}} />
+                    <i className="search icon floated left" onClick={onSearchSubmit}/>
+                </div>
+            </form>
         </div>
-    </div>
     )
 }
 
